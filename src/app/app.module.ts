2e4387b4ea8ab67a491604a2afb2b04406577fcc
kppetrick <kppetrick@gmail.com>
2021-01-28 21:17:10 -0500

    account game and history routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,38 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { AccountComponent } from './account/account.component';
+import { GameComponent } from './game/game.component';
+import { HistoryComponent } from './history/history.component';
+import {RouterModule, Routes} from "@angular/router"
 
+const appRoutes: Routes = [
+  {
+    path:'account',
+    component:AccountComponent
+  },
+  {
+    path:'game',
+    component:GameComponent
+  },
+  {
+    path:'history',
+    component:HistoryComponent
+  }
+]
+// @ts-ignore
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    AccountComponent,
+    GameComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
